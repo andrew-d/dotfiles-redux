26b5a70bd0697636e3440a0af80b40e653d94003
Andrew Dunham <andrew@du.nham.ca>
2016-02-13 20:22:40 -0800

    Use sh-realpath from vendor directory

--- bin/install.sh
+++ bin/install.sh
@@ -19,6 +19,10 @@ readonly PROGNAME="$(basename "$PROGPATH")"
 readonly PROGDIR="$(dirname "$PROGPATH")"
 readonly REPO_ROOT="$(cd -P -- "$PROGDIR/../" && printf '%s\n' "$(pwd -P)")"
 
+# Require vendor libraries
+. "$PROGDIR/../vendor/sh-realpath/realpath.sh"
+
+
 ##################################################
 ## LOGGING
 
@@ -290,83 +294,6 @@ join() {
   echo "$arg" | grep -v '^$' | awk "{ printf \"%s$sep\", \$0 }" | sed "s/$sep\$//"
 }
 
-##################################################
-## REALPATH
-##
-## Note: this implementation was taken from here:
-##   https://github.com/mkropat/sh-realpath/blob/master/realpath.sh
-##
-## The following code (until the next '#'-marked
-## section) is under the following MIT license:
-##   https://github.com/mkropat/sh-realpath/blob/master/LICENSE.txt
-
-realpath() {
-    canonicalize_path "$(resolve_symlinks "$1")"
-}
-
-resolve_symlinks() {
-    _resolve_symlinks "$1"
-}
-
-_resolve_symlinks() {
-    _assert_no_path_cycles "$@" || return
-
-    local dir_context path
-    path=$(readlink -- "$1")
-    if [ $? -eq 0 ]; then
-        dir_context=$(dirname -- "$1")
-        _resolve_symlinks "$(_prepend_dir_context_if_necessary "$dir_context" "$path")" "$@"
-    else
-        printf '%s\n' "$1"
-    fi
-}
-
-_prepend_dir_context_if_necessary() {
-    if [ "$1" = . ]; then
-        printf '%s\n' "$2"
-    else
-        _prepend_path_if_relative "$1" "$2"
-    fi
-}
-
-_prepend_path_if_relative() {
-    case "$2" in
-        /* ) printf '%s\n' "$2" ;;
-         * ) printf '%s\n' "$1/$2" ;;
-    esac
-}
-
-_assert_no_path_cycles() {
-    local target path
-
-    target=$1
-    shift
-
-    for path in "$@"; do
-        if [ "$path" = "$target" ]; then
-            return 1
-        fi
-    done
-}
-
-canonicalize_path() {
-    if [ -d "$1" ]; then
-        _canonicalize_dir_path "$1"
-    else
-        _canonicalize_file_path "$1"
-    fi
-}
-
-_canonicalize_dir_path() {
-    (cd "$1" 2>/dev/null && pwd -P)
-}
-
-_canonicalize_file_path() {
-    local dir file
-    dir=$(dirname -- "$1")
-    file=$(basename -- "$1")
-    (cd "$dir" 2>/dev/null && printf '%s/%s\n' "$(pwd -P)" "$file")
-}
 
 ##################################################
 ## HELP AND FLAG-PARSING
